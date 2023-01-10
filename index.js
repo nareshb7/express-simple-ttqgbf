2b63988babbd95578cf6016875f63a3c83eee240
nareshb7 <104125516+nareshb7@users.noreply.github.com>
2023-01-10 12:14:30 +0530

    Added Form for email Authentication

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
+const { error } = require('console');
 const express = require('express');
+const nodemailer = require('nodemailer');
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hiii Express');
+// app.use(express.static('index'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('pages'));
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'nareshsit7@gmail.com',
+    pass: 'xdocuqqhhnlwyllg',
+  },
+});
+const options = {
+  from: 'nareshsit7@gmail.com',
+  to: '',
+  subject: '',
+  text: '',
+};
+app.post('/validate', (req, res) => {
+  console.log(req.body, 'validate');
+  const { email, subject, text } = req.body;
+  options.to = email;
+  options.subject = subject;
+  options.text = text;
+  transporter.sendMail(options, (err, info) => {
+    if (err) {
+      console.log('Error Occured', err);
+      res.send('Error Occured');
+    } else {
+      console.log('Mail is sent');
+      res.send('Email Sent');
+    }
+  });
 });
 
 /*
 Font Awesome Icns link
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous"> 
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 */
 
 app.listen(3010, () => console.log('server is running....'));
-console.log('hii hello');

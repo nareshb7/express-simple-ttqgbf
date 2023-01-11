df2616ce9a99204dbd72bff4ed5837cc77f42adb
nareshb7 <104125516+nareshb7@users.noreply.github.com>
2023-01-11 07:52:05 +0530

    nodeSms => Vonage package installed

--- index.js
+++ index.js
@@ -1,46 +1,35 @@
-const { error } = require('console');
 const express = require('express');
-const nodemailer = require('nodemailer');
+// const Vonage = require('@vonage/server-sdk');
+
 const app = express();
-// app.use(express.static('index'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('pages'));
+// const vonage = new Vonage({
+//   apiKey: '326a45a4',
+//   apiSecret: 'i7eEFPcm07mVxcYl',
+// });
+const { Vonage } = require('@vonage/server-sdk');
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'nareshsit7@gmail.com',
-    pass: 'xdocuqqhhnlwyllg',
-  },
-});
-const options = {
-  from: 'nareshsit7@gmail.com',
-  to: '',
-  subject: '',
-  text: '',
-};
-app.post('/validate', (req, res) => {
-  console.log(req.body, 'validate');
-  const { email, subject, text } = req.body;
-  options.to = email;
-  options.subject = subject;
-  options.text = text;
-  transporter.sendMail(options, (err, info) => {
-    console.log(options, 'options');
-    if (err) {
-      console.log('Error Occured', err.message);
-      res.send('Error Occured');
-    } else {
-      console.log('Mail is sent');
-      res.send('Email Sent');
-    }
-  });
+const vonage = new Vonage({
+  apiKey: '326a45a4',
+  apiSecret: 'i7eEFPcm07mVxcYl',
 });
 
-/*
-Font Awesome Icns link
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-*/
+const from = 'Vonage APIs';
+const to = '919010586402';
+const text = 'A text message sent using the Vonage SMS API';
+
+async function sendSMS() {
+  await vonage.sms
+    .send({ to, from, text })
+    .then((resp) => {
+      console.log('Message sent successfully');
+      console.log(resp);
+    })
+    .catch((err) => {
+      console.log('There was an error sending the messages.');
+      console.error(err);
+    });
+}
+
+sendSMS();
 
 app.listen(3010, () => console.log('server is running....'));
